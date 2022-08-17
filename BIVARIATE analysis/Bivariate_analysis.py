2610e318388bbaeb47cc241e6d86dba9844f4057
afzalmp40 <afzalmp40@gmail.com>
2022-08-18 03:01:49 +0530

    improved notebook and module

--- BIVARIATE analysis/Bivariate_analysis.py
+++ BIVARIATE analysis/Bivariate_analysis.py
@@ -1,14 +1,5 @@
-'''
-Module made for quick BIVARIATE analysis.
-
-Available functions:
-    Correlation:Plot correlation heatmap for a dataframe.
-    
-'''
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 
 def correlation(df, figsize=(15,10), dpi=100):
     '''
@@ -19,22 +10,22 @@ def correlation(df, figsize=(15,10), dpi=100):
         df: a pandas dataframe
         figsize: default(15,10) set figure size
         dpi: default(100) set figure dpi
-        annot: default(True) display annotations {True, False} 
         
     Returns:
         None
         
     '''
-    fig , ax= plt.subplots(1,2, figsize=figsize, dpi=dpi, annot=True)
+    
+    fig , ax= plt.subplots(1,2, figsize=figsize, dpi=dpi)
 
+    # plotting pearson correlation heatmap
     pearson=df.corr()
     ax[0].set_title('pearson')
-    sns.heatmap(pearson, cmap='YlGnBu', square=True, annot=annot, ax=ax[0])
+    sns.heatmap(pearson, cmap='OrRd', square=True, annot=True, ax=ax[0])
 
+    # plotting spearman correlation heatmap
     spearman=df.corr(method='spearman')
     ax[1].set_title('spearman')
-    sns.heatmap(spearman, cmap='YlGnBu', square=True, annot=annot, ax=ax[1])
+    sns.heatmap(spearman, cmap='OrRd', square=True, annot=True, ax=ax[1])
 
     plt.show()
-    
-###########################################################################################
